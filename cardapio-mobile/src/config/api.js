01727107dbc65846ceb54286dbdae18d96bbfaeb
caueeex <soterocaue2@gmail.com>
2026-05-06 21:09:25 -0300

    .

--- cardapio-mobile/src/config/api.js
+++ cardapio-mobile/src/config/api.js
@@ -2,4 +2,4 @@
  * URL base da API (sem barra no final).
  * Ajuste a porta se o Node estiver em outra (ex.: :3000).
  */
-export const API_BASE_URL = 'http://32.192.67.184';
+export const API_BASE_URL = 'http://32.192.67.184:8080';
